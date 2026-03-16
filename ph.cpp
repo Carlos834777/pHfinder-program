8e9bbce9779f984915a9d8464453658db02da841
Carlos834777 <112784486+Carlos834777@users.noreply.github.com>
2026-03-15 23:34:16 -0500

    Fix error management

--- ph.cpp
+++ ph.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <stdexcept>
 using namespace std;
-int main(int argc, char* argv[])
-{
-    double hydronium, pH;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Error: No input provided." << endl;
+        cout << "Usage: findph [concentration] or findph --h" << endl;
+        return 1;
+    }
     string param = argv[1];
-    if(param == "--h" || param == "-help") {
-    	cout<<endl<<"Insert the hydronium ion concentration (H+) to evaluate its pH"<<endl<<"Example: findph 0.03"<<endl<<endl<<"Use --h or -help to see this message"<<endl<<endl<<endl;
-    } else {
-    hydronium = stod(argv[1]);
-    pH = -log10(hydronium);
-    cout << "pH level is " << pH << endl;
-	}
+    if (param == "--h" || param == "-help") {
+        cout << "\nInsert the hydronium ion concentration (H+) to evaluate its pH" << endl;
+        cout << "Example: findph 0.03" << endl;
+        cout << "\nUse --h or -help to see this message\n" << endl;
+        return 0;
+    }
+    try {
+        double hydronium = stod(param);
+        if (hydronium <= 0) {
+            cerr << "Error: Hydronium concentration must be greater than 0." << endl;
+            return 1;
+        }
+        double pH = -log10(hydronium);
+        cout << "pH level is " << pH << endl;
+    } catch (const invalid_argument& e) {
+        cerr << "Error: '" << param << "' is not a valid number." << endl;
+        return 1;
+    } catch (const out_of_range& e) {
+        cerr << "Error: Number is too large or too small." << endl;
+        return 1;
+    }
     return 0;
 }
